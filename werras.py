c36db4fd2e0600f940a195b861409673ac378cb7
shaki <72961134+devshaki@users.noreply.github.com>
2022-04-08 18:13:58 +0300

    Update werras.py

--- werras.py
+++ werras.py
@@ -1,5 +1,4 @@
-
-used = []
+import ctypes
 import win32clipboard
 from selenium import webdriver
 from time import sleep
@@ -9,19 +8,28 @@ import webdriver_manager
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 import pyautogui
+used = []
 a = str(requests.get("http://norvig.com/ngrams/sowpods.txt").content).split("\\n")
-
-pos = 810, 566
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+width, height = screensize
+Wyahas = 1920/width
+Hyahas = 1080/height
+print(Wyahas)
+textpos1 = 810 * Wyahas
+textpos2 = 566 * Hyahas
+writepos1 = 800 * Wyahas
+writepos2 = 1013 * Hyahas
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 def work():
-    pyautogui.doubleClick(pos)
+    pyautogui.doubleClick(textpos1,textpos2)
     pyautogui.keyDown("ctrl")
     pyautogui.press("c")
     pyautogui.keyUp("ctrl")
     win32clipboard.OpenClipboard()
     data = str(win32clipboard.GetClipboardData()).upper()
     win32clipboard.CloseClipboard()
-    pyautogui.click(800,1013)
+    pyautogui.click(writepos1,writepos2)
     for i in range(len(a)):
         if data in a[i]:
             if any(a[i] in s for s in used):
@@ -33,7 +41,7 @@ def work():
                 break
 
 def help():
-    pyautogui.doubleClick(pos)
+    pyautogui.doubleClick(textpos1,textpos2)
     pyautogui.keyDown("ctrl")
     pyautogui.press("c")
     pyautogui.keyUp("ctrl")
@@ -50,8 +58,7 @@ def help():
                 pyautogui.press("Enter")
                 used.append(a[i])
                 break
-def getPos():
-    pos = pyautogui.position()
+
 
 
 driver.get("https://jklm.fun")
@@ -67,8 +74,7 @@ while True:
             help()
         if keyboard.is_pressed('delete'):
             used.clear()
-        if keyboard.is_pressed('f4'):
-            getPos()
+
 
     except:
         pass
