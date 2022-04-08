36098619e73bbf15c04e6e9222033be986b0d8d2
shaki <72961134+devshaki@users.noreply.github.com>
2022-04-08 16:35:00 +0300

    Update werras.py

--- werras.py
+++ werras.py
@@ -1,17 +1,4 @@
-# import urllib.request
-# from urllib.request import Request, urlopen
-# fp = urllib.request.Request('https://jklm.fun/RZMF', headers={'User-Agent': 'Mozilla/5.0'})
-# webpage = urlopen(fp).read()
-# a = str(webpage).split("\\n")
-#
-# for line in a:
-#     print(line)
-#     if '<div class="syllable">' in line:
-#         print(line)
-#         break
-#
-#
-state = "auto"
+
 used = []
 import win32clipboard
 from selenium import webdriver
@@ -22,9 +9,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 import pyautogui
 a = str(requests.get("http://norvig.com/ngrams/sowpods.txt").content).split("\\n")
-# Create the webdriver object. Here the
-# chromedriver is present in the driver
-# folder of the root directory.
+
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 def work():
     pyautogui.doubleClick(810, 566)
@@ -64,17 +49,16 @@ def help():
                 used.append(a[i])
                 break
 
-# get https://www.geeksforgeeks.org/
+
 driver.get("https://jklm.fun")
 
-# Maximize the window and let code stall
-# for 10s to properly maximise the window.
+
 driver.maximize_window()
 
 
-while True:  # making a loop
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('home'):  # if key 'q' is pressed
+while True:  
+    try:  
+        if keyboard.is_pressed('home'): 
             work()
         if keyboard.is_pressed('end'):
             help()
